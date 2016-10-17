3262615152a960f2c23be578b240150843de3729
zhangyingchengqi <zhangyingchengqi@163.com>
2016-10-17 16:20:54 +0800

    删除指定目录

--- task04/task05.js
+++ task04/task05.js
@@ -7,18 +7,18 @@ var fs=require("fs");
 function del(path){
     var stats=fs.statSync(path);
     if(stats.isFile()){  //如果是文件则删除
-        fs.unlink(path);
+        fs.unlinkSync(path);
     }else if(stats.isDirectory()){ //如果你是一个目录
         //先判断是否是一个空目录
         var files=fs.readdirSync(path);
         if(files.length<=0){ //说明是一个空目录
-            fs.rmdir(path);
+            fs.rmdirSync(path);
         }else{ //如果不是一个空目录，则必须先删除目录下的子文件或子目录
             for(var i=0;i<files.length;i++){
                del(path+"/"+files[i]);
             }
             //循环结束，说明该目录下的所有子目录和文件以及删除，那么将自己删除
-            fs.rmdir(path);
+            fs.rmdirSync(path);
         }
     }
 }
